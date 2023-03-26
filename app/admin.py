bbfaaccb5952f1af392ddf699b8dd16ba311e14b
alexeykudimov <work.alexeykudimov@gmail.com>
2023-03-26 22:07:21 +0300

    add claim model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Claim
 
-# Register your models here.
+admin.site.index_title = 'StackForce'
+
+
+@admin.register(Claim)
+class ClaimAdmin(admin.ModelAdmin):
+    list_per_page = 25
+    list_display = ("id", "email", "phone", "created_at")
+    search_fields = ("id", "email", "phone", "created_at")
--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Claim(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
+    type = models.TextField(null=True, blank=True)
+    is_adaptive = models.BooleanField(default=False, null=True, blank=True)
+    state = models.TextField(null=True, blank=True)
+    start_date = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Заявка {f'от {self.email}' if self.email else f'от {self.phone}' if self.phone else f'#{self.id}'}"
+
+    class Meta:
+        db_table = "claims"
+        verbose_name = 'заявку'
+        verbose_name_plural = 'заявки'
